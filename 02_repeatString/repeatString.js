4bee6db6275e25c1d490bd6b577bfa361b6340a3
Clarence Claus <114810043+aureliuscodes@users.noreply.github.com>
2023-09-24 22:10:33 +0100

    01 and 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const constString = string;
+    if (num > 0) {
+        for (i = 0; i < num - 1; i++) {
+            string += constString;
+        }
+        return string;
+    } else if (num === 0) {
+        return '';
+    } else {
+        return 'ERROR'
+    }
+    
 };
 
 // Do not edit below this line
