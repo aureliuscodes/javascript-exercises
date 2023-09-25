8049440e3cf313c47107ebab6267bbaa5a2318ea
Clarence Claus <114810043+aureliuscodes@users.noreply.github.com>
2023-09-25 18:25:52 +0100

    05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let len = 0;
+    if ((Number.isInteger(numOne) && Number.isInteger(numTwo)) && (numOne >= 0 && numTwo > 0)) {
+        if (numOne > numTwo) {
+            len = (numOne - numTwo + 1);
+        } else if (numTwo > numOne) {
+            len = (numTwo - numOne + 1);
+        } 
 
+        for (let i = 0; i < len; i++) {
+            if (numOne > numTwo) {
+                sum += (numTwo + i);
+            } else if (numTwo > numOne) {
+                sum += (numOne + i);
+            }
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
